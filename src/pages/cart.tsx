6e15d060ed4e1c07bbc39e1569a386a71fa5c199
nilson <nilsonrdc@gmail.com>
2023-12-13 13:56:11 -0300

    first commit

---
+++ src/pages/cart.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+
+const Cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Cart</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+      </Head>
+      <h1>Carrinho</h1>
+    </>
+  );
+};
+export default Cart
